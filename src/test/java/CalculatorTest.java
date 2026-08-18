2e620e3ae84027c09ec32a158afc715afd38077e
Aluno <SEU_EMAIL_DO_GITHUB>
2026-08-18 09:23:08 -0300

    Adiciona pipeline CI

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        assertEquals(4, Calculator.add(2, 2));
+    }
+}
