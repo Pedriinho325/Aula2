d35546bf08fd6f63373cfa372baad2378dd6ba67
Aluno <SEU_EMAIL_DO_GITHUB>
2026-08-18 10:05:50 -0300

    Adiciona operação de subtração e teste

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,11 +1,15 @@
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
     @Test
     public void testAdd() {
         assertEquals(4, Calculator.add(2, 2));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, Calculator.subtract(3, 2));
     }
 }
